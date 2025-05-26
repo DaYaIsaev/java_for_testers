83affdb968e7e8465a9d0b61c5ff85994821e1a0
DIsaev <dimasband@yandex.ru>
2025-05-26 19:03:44 +0300

    Добавлены тесты

--- sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -2,10 +2,14 @@ package ru.stqa.geometry.figures;
 
 public class Square {
     public static void printSquareArea (double side){
-        System.out.println(String.format("Площадь квадрата со сторой %f = %f", side, squareArea(side)));
+        System.out.println(String.format("Площадь квадрата со сторой %f = %f", side, area(side)));
     }
 
-    private static double squareArea(double a) {
+    public static double area(double a) {
         return a * a;
+    }
+
+    public static double perimeter(double a) {
+        return 4 * a;
     }
 }
